b1747aafb1eafb61ecbb682b9406129719554328
User <User@25_1>
2025-04-02 08:44:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace N3__YP__Task_2_02._04._2025
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter num (5...10)");
+            float.TryParse(Console.ReadLine() ,out float x );
+            Console.WriteLine((x > 5 & x < 10) ? "да"  :"неизвестное число ");
+        }
+    }
+}
